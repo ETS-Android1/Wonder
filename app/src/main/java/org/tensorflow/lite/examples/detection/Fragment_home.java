53945b7a1aa2c6a46f3b6e8e863015e251fdd0e7
jiyeon <goto090917@gmail.com>
2022-03-23 15:34:46 +0900

    es

--- app/src/main/java/org/tensorflow/lite/examples/detection/Fragment_home.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/Fragment_home.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +24,8 @@ public class Fragment_home extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
 
     public Fragment_home() {
         // Required empty public constructor
