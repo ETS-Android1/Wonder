f3fadc211065d40228073356e4f5edff31b90115
jyeon <yjyshsj0217@gmail.com>
2022-03-30 17:41:52 +0900

    빈화면 추가

---
+++ app/src/main/java/org/tensorflow/lite/examples/detection/Home_shop_detail.java
@@ -0,0 +1,14 @@
+package org.tensorflow.lite.examples.detection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_shop_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_shop_detail);
+    }
+}
