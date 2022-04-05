953ad09f75423ebf2162ebd26a1779be1201cdfb
jiyeon <goto090917@gmail.com>
2022-04-05 22:50:11 +0900

    제품화면

--- app/src/main/java/org/tensorflow/lite/examples/detection/Home_product.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/Home_product.java
@@ -1,90 +1,51 @@
 package org.tensorflow.lite.examples.detection;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.content.Context;
 import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Home_product#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Home_product extends Fragment {
-    MainActivity activity;
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Home_product() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home_product.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home_product newInstance(String param1, String param2) {
-        Home_product fragment = new Home_product();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class Home_product extends AppCompatActivity {
+    private Context mContext;
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private PageAdapter mPageAdapter;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+        setContentView(R.layout.home_product);
 
-    public void onAttach(Context context){
-        super.onAttach(context);
-        activity = (MainActivity) getActivity();
-    }
+        mContext = getApplicationContext();
+        mTabLayout=(TabLayout) findViewById(R.id.tabs);
 
-    public void onDetach(){
-        super.onDetach();
-        activity = null;
-    }
+        mTabLayout.addTab(mTabLayout.newTab().setText("푸드"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("세제"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("페이스"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("바디"));
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v =  inflater.inflate(R.layout.home_product, container, false);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+        mPageAdapter = new PageAdapter(getSupportFragmentManager(),mTabLayout.getTabCount());
+        mViewPager.setAdapter(mPageAdapter);
 
-        //홈화면으로 넘어가는 뒤로가기 버튼
-        ImageButton back_btn = v.findViewById(R.id.back_btn);
-        back_btn.setOnClickListener(new View.OnClickListener(){
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                activity.setFrag(0);
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
-
-        return v;
     }
 }
